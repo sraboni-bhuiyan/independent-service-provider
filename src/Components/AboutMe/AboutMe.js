866ca4d37d710d7c56ed04ea789cfeb810a53c53
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-04-18 03:10:42 +0200

    changes in data

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -9,10 +9,10 @@ const AboutMe = () => {
                 <img className='w-100' src="https://img.freepik.com/free-photo/studio-portrait-successful-young-businesswoman_1262-5844.jpg?t=st=1650242094~exp=1650242694~hmac=4684bbeba93a5de706b93e9f545b8e1e9965ddc6f119b54aae8ef8391b235190&w=1060" alt="" />
             </div>
             <div className='p-3'>
-                <h3>Hello, I'm Sraboni</h3>
+                <h4>Hello, I'm Sraboni</h4>
                 <p>If your life looks great on paper but feels crap in real life, we need to talk. I am a Career Coach, specialising in helping student to balance professional success with their personal need and happiness.</p>
-                <p>It starts with embracing your uniqueness, respecting your body and creating a sense of balance so you can thrive.</p>
                 <p> I can help you enjoy the success you’ve strived so hard to achieve without having to sacrifice your soul to find it.</p>
+                <p>I am a qualified Career Guidance Counsellor with personal experience of career transition, having made two significant career changes in my own life. I started in 2019. I have worked with adult clients across many industries including Finance, Economics, Legal, Insurance, Technology, Aviation, Manufacturing, Education and the Public Sector.</p>
             </div>
         </div>
         </div>
